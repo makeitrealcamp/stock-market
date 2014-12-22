46aa35ad1cad86b9ab9bda5df5428855dfdeb75c
Oscar Rendon <orendon@gmail.com>
2014-12-22 16:54:08 -0500

    Add basic SSE notification

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,23 @@
 class DashboardController < ApplicationController
+  include ActionController::Live
+
   def index
   end
+
+  def events
+    response.headers['Content-Type'] = 'text/event-stream'
+    sse = SSE.new(response.stream, retry: 300, event: "notification")
+
+    begin
+      # Aqui va la logica (puede ser un pub/sub en redis o postgres)
+      if 1 == 1
+        sse.write({ message: 'Hola', date: Time.now })
+      end
+    rescue
+      # log ...
+    ensure
+      sse.close
+    end
+  end
+
 end
